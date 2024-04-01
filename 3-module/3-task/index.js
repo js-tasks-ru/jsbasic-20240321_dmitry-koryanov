1181ec63ca554d8cecd03f721534fabe37a55e6f
dmitry.koryanov <dmitry.koryanov@yandex.ru>
2024-04-01 18:54:51 +0200

    Задачи к занятию 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let splittedArray = str.split('-');
+  
+  return splittedArray[0] +
+    splittedArray
+      .slice(1)
+      .filter(elem => !!elem)
+      .map(elem => elem[0].toUpperCase() + elem.slice(1))
+      .reduce((result, current) => result + current, '');
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let userArray = users.filter(user => user.age <= age).map(user => user.name + ', ' + user.balance + '\n');
+  let resultString = userArray.reduce((result, current) => result + current, '');
+  resultString = resultString.slice(0, resultString.length - 1);
+  return resultString;
 }
