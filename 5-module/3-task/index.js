7d7fe0d4581b00d9f4731d283b1138ac1aeb0d45
dmitry.koryanov <dmitry.koryanov@yandex.ru>
2024-04-13 11:02:39 +0200

    Задача №3 к занятию 5.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+
+  buttonLeft.style.display = 'none';
+
+  let carousel = document.querySelector('.carousel__inner');
+  let carouselOffsetWidth = carousel.offsetWidth;
+
+  let currentPosition = 1;
+
+  buttonRight.addEventListener('click', () =>
+    moveCarousel(-(currentPosition++ * carouselOffsetWidth)));
+
+  buttonLeft.addEventListener('click', () =>
+    moveCarousel(-(--currentPosition * carouselOffsetWidth)));
+
+  function moveCarousel(moveWidth) {
+    carousel.style.transform = `translateX(${moveWidth}px)`;
+
+    if (currentPosition === 4) {
+      buttonRight.style.display = 'none';
+      currentPosition--;
+    } else if (currentPosition === 0) {
+      buttonLeft.style.display = 'none';
+      currentPosition++;
+    } else {
+      buttonLeft.style.display = '';
+      buttonRight.style.display = '';
+    }
+  }
+
 }
